daaa6d0a18659030e0fb442137ef31bc184c9ed2
Gates <lucasgates234@gmail.com>
2018-11-26 08:45:28 -0600

    Added keyboard listening and JPanel to BufferedImage conversion

--- src/ui/PixelCanvasPanel.java
+++ src/ui/PixelCanvasPanel.java
@@ -2,6 +2,7 @@ package ui;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.*;
 import javax.swing.*;
 
 public class PixelCanvasPanel extends JPanel
@@ -10,7 +11,19 @@ public class PixelCanvasPanel extends JPanel
 	{
 		PixelListener listener = new PixelListener(10,10);
 		
+		setFocusable(true);
+		
 		addMouseListener(listener);
 		addMouseMotionListener(listener);
+		addKeyListener(listener);
+	}
+	
+	public BufferedImage getScreenShot()
+	{
+		BufferedImage bi = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+		
+		paint(bi.getGraphics());
+		
+		return bi;
 	}
 }
--- src/ui/PixelListener.java
+++ src/ui/PixelListener.java
@@ -4,7 +4,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class PixelListener implements MouseMotionListener, MouseListener
+public class PixelListener implements MouseMotionListener, MouseListener, KeyListener
 {
 	int xStep;
 	int yStep;
@@ -15,39 +15,43 @@ public class PixelListener implements MouseMotionListener, MouseListener
 		this.yStep = yStep;
 	}
 	
-	public void mouseDragged(MouseEvent e)
+	public void mouseClicked(MouseEvent e)
 	{
 		int x = e.getX();
 		int y = e.getY();
 		
 		Graphics g = ((JPanel)e.getSource()).getGraphics();
 		g.fillRect((x - x % xStep), (y - y%yStep), xStep, yStep);
+		
 	}
-
-	public void mouseMoved(MouseEvent e)
+	
+	public void mouseDragged(MouseEvent e)
 	{
+		mouseClicked(e);
 	}
 
-	public void mouseClicked(MouseEvent e)
+
+	public void keyTyped(KeyEvent arg0)
 	{
-		mouseDragged(e);
+		System.out.println("Currently testing save feature on any key press");
+		
+		
 	}
+	
+	//No action required on these triggers
+	
+	public void mouseMoved(MouseEvent e) {}
 
-	public void mousePressed(MouseEvent e)
-	{
-	}
+	public void mousePressed(MouseEvent e){}
 
-	public void mouseReleased(MouseEvent e)
-	{
+	public void mouseReleased(MouseEvent e){}
 
-	}
-
-	public void mouseEntered(MouseEvent e)
-	{
-	}
-
-	public void mouseExited(MouseEvent e)
-	{
-	}
+	public void mouseEntered(MouseEvent e){}
+	
+	public void mouseExited(MouseEvent e){}
+	
+	public void keyPressed(KeyEvent arg0){}
+	
+	public void keyReleased(KeyEvent arg0){}
 
 }
